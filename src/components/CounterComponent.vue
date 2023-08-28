86452cd4d49b7541eca357555aadb376c8cb765c
wildanjisung <wildanjisung@gmail.com>
2023-08-28 20:53:00 +0700

    add reusable component

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="doIncrement">counter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CounterComponent',
+  methods: {
+    doIncrement() {
+      this.$emit("update-counter")
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <HelloWorld msg="hai prakerja"/>
-    <HelloWorld msg="halo aku yang ke tiga ni"/>
+  <div>
+    <CounterComponent @update-counter="updateCounterValue" />
+    <h1>angak: {{ angka }}</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CounterComponent from '../components/CounterComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    CounterComponent
+  },
+  data() {
+    return {
+      angka: 0
+    }
+  },
+  methods: {
+    updateCounterValue() {
+      this.angka++
+    }
+  },
+  mounted() {
+    console.log(this.angka)
+    console.log("updated value at mounted")
+    this.updateCounterValue()
+    console.log(this.angka)
   }
 }
 </script>
